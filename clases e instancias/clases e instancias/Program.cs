49bfb46a04388b2422d1c0bd1e2be2b2fff17038
Brenda Frias <47353453+brendafrias@users.noreply.github.com>
2019-08-07 00:28:34 -0300

    Demostracion de accesos a clases y metodos

---
+++ clases e instancias/clases e instancias/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace clases_e_instancias
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            ventana calculadora = new ventana();
+
+            calculadora.Cerrar();//se muestra porque es publico
+            calculadora.Maximizar();// se muestra porque es publico
+            //no podemos acceder a minimizar porque es un metodo privado.
+        }
+    }
+
+
+    class ventana
+    {
+        //Atributos: private solo se puede acceder en ese bloque, static puede cambiarse desde cualquier clase,
+        //y public se puede acceder de cualquier parte del programa
+
+        //Metodos suelen ser publicos para ser llamados por otras clases.
+        public void Cerrar()
+        {
+            Console.WriteLine("La ventana se cerro");
+        }
+
+        private void Minimizar()
+        {
+            Console.WriteLine("Se minimizo la ventana");
+        }
+
+        public void Maximizar()
+        {
+            Console.WriteLine("La ventana se maximzo");
+        }
+    }
+}
